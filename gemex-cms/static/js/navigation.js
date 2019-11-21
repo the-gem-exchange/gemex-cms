4eff35685d0ec136e5f49979aa3d887277adc6e7
juan0tron <juandglover@gmail.com>
2019-11-20 20:23:00 -0800

    MVP for mobile traits / nav

---
+++ gemex-cms/static/js/navigation.js
@@ -0,0 +1,49 @@
+var scrollSpeed;
+var transparentNav = false;
+
+function showNav() {
+	$('nav.site-nav').removeClass('hidden');
+}
+
+function hideNav() {
+	$('nav.site-nav').addClass('hidden');
+}
+
+function showHideNav() {
+	$(document).bind('mousewheel DOMMouseScroll', function(e){
+		scrollSpeed = e.originalEvent.wheelDelta || e.originalEvent.detail || 0;
+
+		let down = scrollSpeed >= 1;
+		let up   = scrollSpeed <= -1;
+
+		if (down)   { hideNav(); }
+		else if(up) { showNav(); }
+
+		if($('#content').scrollTop() == 0 && transparentNav){
+			$('nav.site-nav').addClass('transparent');
+		}
+		else{
+			$('nav.site-nav').removeClass('transparent');
+		}
+	});
+}
+
+function toggleMobileMenu() {
+	$('.toggle-mobile-menu').add('.mobile-overlay').on('click', function(e){
+		e.preventDefault();
+		$('.mobile-nav').toggleClass('active');
+		$('.mobile-overlay').toggleClass('active');
+	});
+}
+
+$(document).ready(function(){
+	showHideNav();
+	toggleMobileMenu();
+
+	if($('#homepage-content').length || $('.comicpage').length){
+		transparentNav = true;
+	}
+	else{
+		$('nav.site-nav').removeClass('transparent');
+	}
+})

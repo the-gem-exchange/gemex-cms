88f75f293996e4a79cf63bc0bf430c32d51ca694
Jon Glover <glojonat@amazon.com>
2019-11-20 21:31:05 -0800

    Turn S3 media back on

--- gemex-cms/settings/dev.py
+++ gemex-cms/settings/dev.py
@@ -9,18 +9,18 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # AWS Settings
-# AWS_STORAGE_BUCKET_NAME = 'static.thegemexchange.net'
-# AWS_S3_REGION_NAME = 'us-east-1'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = False
-# AWS_HEADERS = {
-#     'Access-Control-Allow-Origin': '*'
-# }
+AWS_STORAGE_BUCKET_NAME = 'static.thegemexchange.net'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = False
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
 
 # Media Storage
 MEDIAFILES_LOCATION = 'media'
-# MEDIA_URL = "https://s3-{}.amazonaws.com/{}/{}/".format(AWS_S3_REGION_NAME, AWS_STORAGE_BUCKET_NAME, MEDIAFILES_LOCATION)
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIA_URL = "https://s3-{}.amazonaws.com/{}/{}/".format(AWS_S3_REGION_NAME, AWS_STORAGE_BUCKET_NAME, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 try:
     from .local import *

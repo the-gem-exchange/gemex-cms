523f343c5a1f46fa30d90eddc3f5a9f3613b923d
Jon Glover <glojonat@amazon.com>
2019-11-21 21:29:03 -0800

    Update S3 settings

--- gemex-cms/settings/dev.py
+++ gemex-cms/settings/dev.py
@@ -12,10 +12,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AWS_STORAGE_BUCKET_NAME = 'static.thegemexchange.net'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = False
+AWS_DEFAULT_ACL = None
 AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*'
 }
+AWS_QUERYSTRING_EXPIRE = '604800'
+AWS_DEFAULT_ACL = None
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_SECURE_URLS = False
 
 # Media Storage
 MEDIAFILES_LOCATION = 'media'

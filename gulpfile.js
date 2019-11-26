8f086d531772128a78527fa02c4061348f5646df
juan0tron <juandglover@gmail.com>
2019-11-25 20:05:01 -0800

    Add gulp-autoprefixer for better cross-browser CSS compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 'use strict';
 
-const gulp  = require('gulp');
-const sass  = require('gulp-sass');
-const csso  = require('gulp-csso');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass:main', function () {
 	return gulp.src('./gemex-cms/static/scss/styles.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(csso()) // Minify CSS
+		.pipe(autoprefixer({ cascade: false })) // add vendor prefixes
+		.pipe(csso()) // minify
 		.pipe(gulp.dest('./gemex-cms/static/css/'));
 });
 
 gulp.task('sass:admin', function () {
 	return gulp.src('./gemex-cms/static/scss/admin/admin.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(csso()) // Minify CSS
+		.pipe(autoprefixer({ cascade: false })) // add vendor prefixes
+		.pipe(csso()) // minify
 		.pipe(gulp.dest('./gemex-cms/static/css/'));
 });
 

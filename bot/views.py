0cc492090791e852daa677880fb0a3d482aeb82d
juan0tron <juandglover@gmail.com>
2020-01-26 16:31:39 -0800

    Update message preview for !commands in admin to show all commands

--- bot/views.py
+++ bot/views.py
@@ -1,26 +1,8 @@
-from .models import BotCommand
+from .models import BotCommand, get_all_commands
 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.forms.models import model_to_dict
-
-def get_all_commands():
-	commands = BotCommand.objects.order_by('command').all() # All commands in alphabetical order
-	reply    = ''
-
-	# Try to get the intro text message from the site, otherwise fall back to a standard intro
-	try:
-		commands_message = BotCommand.objects.get(command='commands')
-		if commands_message:
-			reply += commands_message.text + '\\n\\n'
-	except:
-		reply += 'Available commands for Discord Chan:\\n\\n'
-
-	# Build the list in markdown with newline characters
-	for c in commands:
-		reply += c._command()+'\\n'
-
-	return reply
 
 def get_command(request, command):
 	if command == 'commands':

657e0f7865839610fa6702eb1789e23d4894a409
juan0tron <juandglover@gmail.com>
2020-01-26 17:03:29 -0800

    Use HTTPS for static.thegemexchange.net

--- gemex-cms/settings/dev.py
+++ gemex-cms/settings/dev.py
@@ -13,13 +13,13 @@ AWS_STORAGE_BUCKET_NAME = 'static.thegemexchange.net'
 AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*'
+	'Access-Control-Allow-Origin': '*'
 }
 AWS_QUERYSTRING_EXPIRE = '604800'
-AWS_DEFAULT_ACL = None
-AWS_S3_SECURE_URLS = False
+AWS_S3_SECURE_URLS = True
+AWS_S3_USE_SSL = True
 
 # Media Storage
 MEDIAFILES_LOCATION = 'media'
@@ -28,6 +28,6 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 try:
-    from .local import *
+	from .local import *
 except ImportError:
-    pass
+	pass

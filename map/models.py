0ed0ddf22198c5b3d8b4a5f9aa9c4f49f3ed3f33
juan0tron <glojonat@38f9d3e6d88b.ant.amazon.com>
2020-02-02 14:52:52 -0800

    Create multiple node types/styles: IE Continent, Ocean, Hidden

--- map/models.py
+++ map/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.html import format_html
 
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 
-from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, HelpPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Orderable
@@ -31,11 +32,13 @@ class MapSettings(ClusterableModel, BaseSetting):
 
 @register_snippet
 class MapLocation(index.Indexed, ClusterableModel):
+
 	title       = models.CharField(max_length=255, help_text="The name as it appears on the map.")
 	description = RichTextField(blank=True)
-	node_html   = models.TextField(
-		blank=True,
-	)
+
+	node_html = models.TextField(blank=True)
+	rotate    = models.CharField(blank=True, max_length=10, help_text="Rotates the icon/title on the map, in degrees (180, -45, 90, etc)")
+
 	node_image  = models.ForeignKey(
 		'image.CustomImage',
 		null=True,
@@ -53,19 +56,62 @@ class MapLocation(index.Indexed, ClusterableModel):
 		help_text="A transparent layer over the whole map that is related to this location."
 	)
 
-	type = [
-		('continent', 'Continent'),
-		('ocean',     'Ocean'),
-		('hidden',    'Hidden (No dot)')
-	]
+	type = models.CharField(
+		choices=[
+			('standard',  'Standard'),
+			('continent', 'Continent'),
+			('ocean',     'Ocean'),
+			('hidden',    'Hidden'),
+			('none',      'Not on map'),
+		],
+		null=True,
+		blank=False,
+		default='standard',
+		max_length=255
+	)
 
 	def __str__(self):
 		return self.title
 
+	def _title(self):
+		return format_html(
+			'<b style="min-width: 200px;">{}</b>',
+			self.title
+		)
+
+	def _description(self):
+		return format_html(
+			'<div style="max-width:500px;">{}</div>',
+			format_html(self.description)
+		)
+
+	def _image(self):
+		if self.node_image:
+			return format_html(
+				'<img class="species-thumbnail" src="{}" />',
+				self.node_image.file.url,
+			)
+		else:
+			return None
+
+	def _overlay(self):
+		if self.overlay_image:
+			return format_html(
+				'<img style="max-width:300px; border: 1px solid #333;" src="{}" />',
+				self.overlay_image.file.url,
+			)
+		else:
+			return None
+
 	panels = [
 		FieldPanel('title'),
+		FieldPanel('type'),
 		FieldPanel('description'),
-		FieldPanel('node_html'),
+		FieldPanel('rotate'),
+
+		FieldPanel('node_html', classname="code-editor"),
+		HelpPanel(content='HTML can be generated <a target="_blank" href="https://www.inabrains.com/tooltip/image-hotspot-creator.html">here</a>'),
+
 		ImageChooserPanel('node_image'),
 		ImageChooserPanel('overlay_image'),
 	]
@@ -74,8 +120,8 @@ from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 class MapLocationAdmin(ModelAdmin):
 	model         = MapLocation
 	menu_icon     = 'site'
-	list_display  = ('title', 'description')
-	search_fields = ['title', 'description']
-	list_display_add_buttons = 'title'
+	list_display  = ('_image', '_title', 'type', '_description', '_overlay')
+	search_fields = ['title', 'description', 'type']
+	list_display_add_buttons = '_title'
 
 modeladmin_register(MapLocationAdmin)

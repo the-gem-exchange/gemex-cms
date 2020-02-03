be9cb6215af99c9096d4b6deca76fa5ab8f672e0
juan0tron <juandglover@gmail.com>
2020-02-02 21:21:19 -0800

    Make location data visible again

--- gemex-cms/settings/base.py
+++ gemex-cms/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 from config import *
 
-VERSION = '2.6.6'
+VERSION = '2.6.7'
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)

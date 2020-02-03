e2536efda1a9f5a01d447a6ed1c14f80b7277495
juan0tron <juandglover@gmail.com>
2020-02-02 16:46:52 -0800

    Add "text" node type

--- gemex-cms/settings/base.py
+++ gemex-cms/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 from config import *
 
-VERSION = '2.6.1'
+VERSION = '2.6.6'
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
